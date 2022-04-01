c89349d471a7c48e4d39c77e6ce7c5b015a342bf
Arun Murali <arun.murali@secondopinionexpert.com>
2022-04-01 12:16:59 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import reactDOM from "react-dom";
+const template = <h1>Hello World!!</h1>;
+reactDOM.render(template, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: { path: path.join(__dirname, "public"), filename: "bundle.js" },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+  },
+  //   options: { devtools: "cheap-module-eval-source-map" },
+};
